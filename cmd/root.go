acdb69eb0beb44ef69f0663ace72a2e96df10d88
ofir matuti <ofirmatuti@gmail.com>
2022-07-25 10:03:38 +0300

    extend flags

--- cmd/root.go
+++ cmd/root.go
@@ -7,9 +7,8 @@ import (
 	"fmt"
 	"os"
 
-	cmdconfig "github.com/ofirmatuti/vt/cmd/config"
-	cmdlogin "github.com/ofirmatuti/vt/cmd/login"
-
+	cmdConfig "github.com/ofirmatuti/vt/cmd/config"
+	cmdLogin "github.com/ofirmatuti/vt/cmd/login"
 	"github.com/spf13/cobra"
 )
 
@@ -19,9 +18,13 @@ var rootCmd = &cobra.Command{
 
 	Run: func(cmd *cobra.Command, args []string) {
 		isTtl, _ := cmd.Flags().GetBool("ttl")
-
+		updateCache, _ := cmd.Flags().GetBool("update-cache")
 		if isTtl {
 			fmt.Println("Hey! i am vt -t !") //TODO add logic
+			os.Exit(0)
+		}
+		if updateCache {
+			fmt.Println("Hey! i am updating the cache now")
 			os.Exit(0)
 		}
 
@@ -33,12 +36,17 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		_, err := fmt.Fprintln(os.Stderr, err)
+		if err != nil {
+			fmt.Println("error printing cmd stderr")
+		}
 		os.Exit(1)
 	}
 }
 
 func init() {
-	rootCmd.Flags().BoolP("ttl", "t", false, "Prints ttl for vault okta authentication")
-	rootCmd.AddCommand(cmdconfig.NewCmdConfig())
-	rootCmd.AddCommand(cmdlogin.NewCmdLogin())
+	rootCmd.Flags().BoolP("ttl", "t", false, "Prints authentication token ttl")
+	rootCmd.Flags().BoolP("update-cache", "f", false, "Fetches all basedir subdirectories recursively to cache")
+	rootCmd.AddCommand(cmdConfig.NewCmdConfig())
+	rootCmd.AddCommand(cmdLogin.NewCmdLogin())
 }
